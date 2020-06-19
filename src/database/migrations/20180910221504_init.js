5945a5361f5c096dc476c6229ab4bf656a1896ec
Christian Tavares <christian.tavares@nave.rs>
2020-06-19 09:02:32 -0300

    feat: add migrations at project
    Signed-off-by: Christian Tavares <christian.tavares@nave.rs>

--- src/database/migrations/20180910221504_init.js
+++ src/database/migrations/20180910221504_init.js
@@ -1,26 +1,34 @@
 export const up = knex =>
   knex.schema
-    .createTable('roles', table => {
-      table.increments('id').primary()
-      table.string('role').notNullable()
-    })
-    .createTable('users', table => {
+    .createTable('projects', table => {
       table.uuid('id').primary()
       table.string('name').notNullable()
+      table.timestamps(true, true)
+    })
+    .createTable('navers', table => {
+      table.uuid('id').primary()
+      table.string('name').notNullable()
+      table.integer('age')
+      table.datetime('admission_date').notNullable()
+      table.string('job_role')
+    })
+    .createTable('navers_projects', table => {
+      table.increments('id').primary()
+      table.uuid('naver_id')
       table
-        .specificType('email', 'CITEXT')
-        .unique()
-        .notNullable()
-      table.string('password').notNullable()
-      table.string('password_reset_token').unique()
-      table.integer('role').unsigned()
-      table
-        .foreign('role')
+        .foreign('naver_id')
         .references('id')
-        .inTable('roles')
-        .onDelete('CASCADE')
+        .inTable('navers')
+      table.uuid('project_id')
+      table
+        .foreign('project_id')
+        .references('id')
+        .inTable('projects')
       table.timestamps(true, true)
     })
 
 export const down = knex =>
-  knex.schema.dropTableIfExists('users').dropTableIfExists('roles')
+  knex.schema
+    .dropTableIfExists('navers_projects')
+    .dropTableIfExists('navers')
+    .dropTableIfExists('projects')
